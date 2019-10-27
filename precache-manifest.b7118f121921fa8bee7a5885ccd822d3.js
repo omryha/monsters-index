446078f31f898d7458b664f347df1c051ae12373
omryha <omry.hazut@mail.huji.ac.il>
2019-10-27 12:01:52 +0200

    Updates

---
+++ precache-manifest.b7118f121921fa8bee7a5885ccd822d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b98cf7ce32c15b9007e4db6cba1b94be",
+    "url": "/monsters-index/index.html"
+  },
+  {
+    "revision": "a5e82c010907cc5ff2cc",
+    "url": "/monsters-index/static/css/main.edf0bb0d.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-index/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "a5e82c010907cc5ff2cc",
+    "url": "/monsters-index/static/js/main.fa2ff71a.chunk.js"
+  },
+  {
+    "revision": "af5209cbc8a03c110f74",
+    "url": "/monsters-index/static/js/runtime-main.3ff723c9.js"
+  }
+]);
